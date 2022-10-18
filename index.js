9a84054bf91b7572a0706fdca78a7d07df7d07ea
James Gabrielsen <jgabrielsen@users.noreply.github.com>
2022-10-18 19:21:26 +0000

    updated Intents syntax for discord.js v14

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const token = process.env['token'];
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
